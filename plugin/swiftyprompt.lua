a5954eba83016a7280921e4da153329c0797e5a0
Mihail Anghelici <seseriusca@gmail.com>
2026-09-23 18:05:08 -0400

    Spawn window at cursor or middle of bloc selection (#1)
    Closes #1.

--- plugin/swiftyprompt.lua
+++ plugin/swiftyprompt.lua
@@ -5,9 +5,13 @@ end
 vim.g.loaded_swiftyprompt = 1
 
 vim.api.nvim_create_user_command("SwiftPromptPop", function()
-  require("swiftyprompt").open_window()
+  require("swiftyprompt").open_at_cursor()
 end, {})
 
 vim.keymap.set("n", "<leader>aa", function()
-  require("swiftyprompt").open_window()
+  require("swiftyprompt").open_at_cursor()
 end, { desc = "Open SwiftPrompt" })
+
+vim.keymap.set("x", "<leader>aa", function()
+  require("swiftyprompt").open_at_visual_selection()
+end, { desc = "Open SwiftPrompt for selection" })
